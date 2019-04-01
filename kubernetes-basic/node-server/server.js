9ce784c62a47bf7376c11a4c23da4f79d738a276
Szanto Peter <github@szantocsalad.hu>
2019-04-01 02:03:51 +0200

    Adding node server

---
+++ kubernetes-basic/node-server/server.js
@@ -0,0 +1,16 @@
+var os = require("os");
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`{"host" : "${os.hostname()}", "type" : "${os.type()}", "platform" : "${os.platform()}"}`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ kubernetes-basic/step1.md
@@ -0,0 +1,3 @@
+#Build and run server
+Change to server directory `cd node-server`{{execute}}
+Build docker image `cd docker build -t exampledriven/node-server .`{{execute}}
